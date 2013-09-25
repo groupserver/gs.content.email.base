0a2affa3e2be324472340caee4b5f91de578b070
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-25 11:37:11 -0700

    Adding the SiteEmail and GroupEmail classes

--- gs/content/email/base/__init__.py
+++ gs/content/email/base/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import  # --=mpj17=-- This is quite important
 #lint:disable
-from email import SiteEmail, GroupEmail
+from .email import SiteEmail, GroupEmail  # "email" is a standard module
 #lint:enable
--- gs/content/email/base/email.py
+++ gs/content/email/base/email.py
@@ -1,9 +1,38 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from premailer import transform
+from zope.cachedescriptors.property import Lazy
+from zope.component import createObject
+from gs.content.baes import SitePage
 
 
-class SiteEmail(object):
-    pass
+class SiteEmail(SitePage):
+    def __init__(self, context, request):
+        super(SiteEmail, self).__init__(context, request)
+
+    def __call__(self):
+        orig = super(SiteEmail, self).__call__()
+        retval = transform(orig)
+        return retval
 
 
-class GroupEmail(object):
-    pass
+class GroupEmail(SiteEmail):
+    def __init__(self, context, request):
+        super(GroupEmail, self).__init__(context, request)
+
+    @Lazy
+    def groupInfo(self):
+        retval = createObject('groupserver.GroupInfo', self.group)
+        return retval
