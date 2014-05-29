d40a4a67418efa7aabd957e2c10da725262a0fbe
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-29 12:08:51 -0700

    Adding some unit tests.

---
+++ gs/content/email/base/tests/email.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
+from mock import MagicMock
+from unittest import TestCase
+from zope.publisher.browser import TestRequest
+from gs.content.email.base.email import SiteEmail
+import gs.content.email.base.email
+
+
+class TestSiteEmailBase(TestCase):
+    'Test the SiteEmail.base attribute'
+
+    def test_base_url_awol(self):
+        'Test the base-attribute when the URL is missing'
+        request = TestRequest()
+        siteEmail = SiteEmail(None, request)
+        r = siteEmail.base
+        self.assertEqual('', r)
+
+    def test_base_url_index(self):
+        'Test the base attribute when the URL ends with index.html'
+        request = TestRequest(
+            ACTUAL_URL='http://groups.example.com/email/index.html',
+            URL='http://groups.example.com/email/index.html',
+            URL1='http://groups.example.com/email')
+        siteEmail = SiteEmail(None, request)
+
+        r = siteEmail.base
+        self.assertEqual('http://groups.example.com/email', r)
+
+    def test_base_url_page(self):
+        'Test the base attribute when the URL ends with page.html'
+        request = TestRequest(
+            ACTUAL_URL='http://groups.example.com/email/page.html',
+            URL='http://groups.example.com/email/page.html',
+            URL1='http://groups.example.com/email')
+        siteEmail = SiteEmail(None, request)
+
+        r = siteEmail.base
+        self.assertEqual('http://groups.example.com/email', r)
+
+
+class TestSiteEmailRemoveStyle(TestCase):
+    'Test the SiteEmail.remove_style method'
+    withStyle = '''<html>
+    <head>
+        <title>Ethyl the frog</title>
+        <style>p {font-weight:bold;}</style>
+    </head>
+    <body>
+        <p>Violence</p>
+    </body>
+</html>'''
+
+    withoutStyle = '''<html>
+    <head>
+        <title>Ethyl the frog</title>
+    </head>
+    <body>
+        <p>Violence</p>
+    </body>
+</html>'''
+
+    withStyleAttr = '''<html>
+    <head>
+        <title>Ethyl the frog</title>
+    </head>
+    <body>
+        <p style="font-weight:bold">Violence</p>
+    </body>
+</html>'''
+
+    @staticmethod
+    def discard_whitespace(t):
+        retval = t.replace('\n', '').replace(' ', '')
+        return retval
+
+    def test_remove_style(self):
+        'Test the removal of the <style> element'
+        siteEmail = SiteEmail(None, None)
+        r = siteEmail.remove_style_elements(self.withStyle)
+        self.assertEqual(self.discard_whitespace(self.withoutStyle),
+                            self.discard_whitespace(r))
+
+    def test_remove_style_no_style(self):
+        'Ensure that things are ok if there is no <style> element'
+        siteEmail = SiteEmail(None, None)
+        r = siteEmail.remove_style_elements(self.withoutStyle)
+        self.assertEqual(self.withoutStyle, r)
+
+    def test_call(self):
+        gs.content.email.base.email.SitePage.__call__ = MagicMock(
+                                                return_value=self.withStyle)
+        request = TestRequest(
+            ACTUAL_URL='http://groups.example.com/email/page.html',
+            URL='http://groups.example.com/email/page.html',
+            URL1='http://groups.example.com/email')
+        siteEmail = SiteEmail(None, request)
+        r = siteEmail()
+        self.assertEqual(self.discard_whitespace(self.withStyleAttr),
+                            self.discard_whitespace(r))
+
+
+class TestGroupEmail(TestCase):
+    'Test the GroupEmail view'
---
+++ gs/content/email/base/tests/text.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
+from mock import MagicMock
+from unittest import TestCase
+from gs.content.email.base.text import TextMixin
+
+
+class TestTextMixinFill(TestCase):
+    'Test the fill method of the TestMixin class'
+
+    def setUp(self):
+        self.textMixin = TextMixin()
+
+    @staticmethod
+    def one_line(t):
+        'Write the text, ``t``, on one (1) line.'
+        retval = t.replace('\n', ' ')
+        return retval
+
+    def test_fill(self):
+        'Test a normal run of the fill class-method'
+        longText = '''Glib's Fourth Law of Unreliability:
+
+Investment in reliability will increase until it exceeds the probable cost of
+errors, or until someone insists on gettingsome useful work done.'''
+        r = self.textMixin.fill(longText)
+
+        self.assertEqual(self.one_line(longText), self.one_line(r))
+        for line in r.split('\n'):
+            self.assertLessEqual(74, len(r))
+
+    def test_fill_empty(self):
+        'Test the fill method when there is no text to wrap'
+        r = self.textMixin.fill('')
+        self.assertEqual('', r)
+
+
+class TestTextMixinHeader(TestCase):
+    'Test the set_header method TestMixin class'
+    filename = 'ethyl_the_frog.txt'
+
+    def setUp(self):
+        self.textMixin = TextMixin()
+
+    def test_set_header(self):
+        'Test a normal call of set_header'
+        self.textMixin.request = MagicMock()
+        self.textMixin.set_header(self.filename)
+        callArgs = self.textMixin.request.response.setHeader.call_args_list
+        self.assertEqual(2, len(callArgs))
+        headers = [c[0][0] for c in callArgs]
+        self.assertIn('Content-Type', headers)
+        self.assertIn('Content-Disposition', headers)
+
+    def test_set_header_no_request(self):
+        'Test a call to set_header when there is no request'
+        self.assertRaises(ValueError, self.textMixin.set_header, self.filename)
+
+    def test_set_header_no_response(self):
+        'Test a call to set_header when there is no request.response'
+        self.textMixin.request = None
+        self.assertRaises(ValueError, self.textMixin.set_header, self.filename)
+
+    def test_set_header_unicode(self):
+        'Test a call to set_header when some Unicode is passed in'
+        self.textMixin.request = MagicMock()
+        self.textMixin.set_header(self.filename + '…')
+        args, kwargs = self.textMixin.request.response.setHeader.call_args
+        self.assertNotIn('…', args[1])
